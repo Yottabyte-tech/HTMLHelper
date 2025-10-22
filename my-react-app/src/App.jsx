36c3d473ad0ad8911d044ec69870969336d29a8f
Yottabyte-tech <callanchem314@gmail.com>
2025-10-22 01:58:41 +0000

    Init

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+function App() {
+  return (
+    <>
+      <h1 style={{}}>Text Workspace</h1>
+      <div id='full-workspace'>
+        <div id='sidebar'></div>
+        <div id='workspace'></div>
+      </div>
+
+      <div id='text-display'></div>
+    </>
+  );
+}
+
+export default App;
